a94c9f925d3ed50c744c6161c1eb4fc6b0f1d019
Laxmikant <laxmikant.anvekar@mobiliya.com>
2020-06-05 16:01:27 +0530

    changes

--- app/src/main/java/com/undp/fleettracker/constants/NetworkConstants.kt
+++ app/src/main/java/com/undp/fleettracker/constants/NetworkConstants.kt
@@ -15,5 +15,5 @@ const val AD_TENANT_ID: String = "b3e5db5e-2944-4837-99f5-7488ace54319"
 const val AD_CLIENT_ID = "46ef45fa-dc03-45e0-a709-e16e0c9df6a6"
 
 // AppCenter crashlytics key
-const val APP_CENTER_SECRET_KEY = "d6f7d27c-8264-410e-9cf0-3bba42f3b3ae" //"9d9b7426-4d23-46ed-827d-5d021d17d765"
+const val APP_CENTER_SECRET_KEY = "8389ee56-f54e-40e3-8afa-f605cc9e8fb8" //"9d9b7426-4d23-46ed-827d-5d021d17d765"
 
